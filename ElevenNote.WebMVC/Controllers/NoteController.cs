719b75ef05fbbae245df75e091276450ca149886
jordan stoner <jordstoner@gmail.com>
2021-05-03 10:06:55 -0400

    initial commit

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,51 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var UserId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(UserId);
+            var model = service.GetNotes();
+            return View(model);
+ 
+        }
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            NoteService service = CreateNotService();
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
+        }
+
+        private NoteService CreateNotService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
+    }
+}
